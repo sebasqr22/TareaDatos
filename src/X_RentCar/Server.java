a5459048e2ba6eff911450ad3be149ec663cd200
shaky0907 <shaky0907@users.noreply.github.com>
2020-10-09 21:54:46 -0600

    Fixed issues

---
+++ src/X_RentCar/Server.java
@@ -0,0 +1,36 @@
+package X_RentCar;
+
+public class Server {
+
+	Prestador[] prestadores = new Prestador[100];
+	int iP = 0;
+	int iC = 0;
+	// partiendo de que en la aplicacion real ambas clases existen en el codigo
+	Cliente[] clientes = new Cliente[100];
+
+	public Prestador[] getPrestadores() {
+
+		return prestadores;
+	}
+
+	public void setPrestadores(Prestador presta) {
+
+		this.prestadores[iP] = presta;
+		this.iP++;
+	}
+
+	public Cliente[] getClientes() {
+
+		return clientes;
+	}
+
+	public void setClientes(Cliente cliente) {
+
+		this.clientes[iC] += cliente;
+		this.iC++;
+	}
+		
+
+
+
+}

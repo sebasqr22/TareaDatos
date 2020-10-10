3aafdc8de43727642880d7cbf419bb8d0b407aa0
sebasqr22 <63075293+sebasqr22@users.noreply.github.com>
2020-10-09 21:42:45 -0600

    Servidor

--- src/X_RentCar/Server.java
+++ src/X_RentCar/Server.java
@@ -4,7 +4,40 @@ public class Server {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Prestador[] prestadores;
+								// partiendo de que en la aplicacion real ambas clases existen en el codigo
+		Cliente[] clientes;
 
+	}
+	
+	class Informacion{
+		
+		public Prestador[] getPrestadores() {
+			
+			return prestadores;
+		}
+		
+		public void setPrestadores(Prestador[] presta) {
+			
+			this.prestadores += presta;
+		}
+		
+		public Cliente[] getClientes() {
+			
+			return clientes;
+		}
+		
+		public void setClientes(Cliente[] cliente) {
+			
+			this.clientes += cliente;
+		}
+		
+		
+		Prestador[] prestadores;
+				// partiendo de que en la aplicacion real ambas clases existen en el codigo
+		Cliente[] clientes;
+		
 	}
 
 }
